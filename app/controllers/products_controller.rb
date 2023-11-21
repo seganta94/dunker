56ba49b7d2275ca2c6adf9046351b8b3c9bb6c9b
stefany santos <stefany.chsantos@gmail.com>
2023-11-21 17:56:04 +0000

    form products view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,7 +38,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :category, :brand, :condition, :price, :photo)
+    params.require(:product).permit(:name, :category, :brand, :condition, :price, :sport, :description, :photo)
   end
 
   def set_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,16 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
+  CATEGORIES = ['Apparel', 'Accessories', 'Equipment', 'Shoes', 'Adult', "Kids"]
+  BRANDS = ['Nike', 'Adidas', 'Reebok', 'Under Armor', 'Puma', 'Asics', 'New Balance', 'Speedo', 'Mizuno']
+  CONDITION = ['New', 'Used', 'Refurbished']
+  SPORTS = ['Soccer', 'Footbal', 'Tennis', 'Volleyball', 'Skateboarding', 'Hockey', 'Snowboarding', 'Ski', 'Rollerblading', 'Basketball', 'Golf', 'Hugby']
+
   validates :name, presence: true
-  validates :category, presence: true, inclusion: { in: ['Apparel', 'Accessories', 'Equipment', 'Shoes', 'Adult', "Kids"] }
-  validates :brand, presence: true, inclusion: { in: ['Nike', 'Adidas', 'Reebok', 'Under Armor', 'Puma', 'Asics', 'New Balance', 'Speedo', 'Mizuno'] }
-  validates :condition, presence: true, inclusion: { in: ['New', 'Used', 'Refurbished'] }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  validates :brand, presence: true, inclusion: { in: BRANDS }
+  validates :condition, presence: true, inclusion: { in: CONDITION }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true,  length: { maximum: 150 }
-  validates :sport, presence: true, inclusion: { in: ['Soccer', 'Footbal', 'Tennis', 'Volleyball', 'Skateboarding', 'Hockey', 'Snowboarding', 'Ski', 'Rollerblading', 'Basketball', 'Golf', 'Hugby'] }
+  validates :sport, presence: true, inclusion: { in: SPORTS }
 end
