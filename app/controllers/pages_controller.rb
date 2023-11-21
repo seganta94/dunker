1b62b47592f6efb86f1b0dd692d967335fb39e2c
Eduardo Segantine <eduardo.segantine@gmail.com>
2023-11-21 14:27:42 -0300

    user view corrected

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,2 @@
 class PagesController < ApplicationController
-  def home
-  end
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  # end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,4 +45,9 @@ class ProductsController < ApplicationController
   def set_product
     @product = Product.find(params[:id])
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end
