575282a6f65b4db5ce4dd093721c81b70bee1f0d
Felipi Freo <psicofelipi@gmail.com>
2023-11-23 17:01:38 -0300

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,36 +17,36 @@ puts "Seeding products..."
 10.times do
 product = Product.create!(user_id: user.id, name: Faker::Sport.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price,description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
 
-  # image_url = "https://source.unsplash.com/400x400/?sports"
-  # product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
+  image_url = "https://source.unsplash.com/400x400/?sports"
+  product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 end
 
 10.times do
   product = Product.create!(user_id: user2.id, name: Faker::Sport.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price,description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
 
-  # image_url = "https://source.unsplash.com/400x400/?sports"
-  # product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
+  image_url = "https://source.unsplash.com/400x400/?sports"
+  product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 end
 
 10.times do
   product = Product.create!(user_id: user3.id, name: Faker::Sport.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price,description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
 
-  # image_url = "https://source.unsplash.com/400x400/?sports"
-  # product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
+  image_url = "https://source.unsplash.com/400x400/?sports"
+  product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 end
 
 10.times do
   product = Product.create!(user_id: user4.id, name: Faker::Sport.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price,description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
 
-  # image_url = "https://source.unsplash.com/400x400/?sports"
-  # product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
+  image_url = "https://source.unsplash.com/400x400/?sports"
+  product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 end
 
 10.times do
   product = Product.create!(user_id: user5.id, name: Faker::Sport.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price,description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
 
-  # image_url = "https://source.unsplash.com/400x400/?sports"
-  # product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
+  image_url = "https://source.unsplash.com/400x400/?sports"
+  product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 end
 
 puts 'Seeding completed!'
