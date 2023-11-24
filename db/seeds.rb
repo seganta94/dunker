4fde462186b692741998c58075d4b7b6e0ef311e
stefany santos <stefany.chsantos@gmail.com>
2023-11-24 16:12:21 -0300

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,38 +27,65 @@ user5 = User.create(first_name: "adriano", last_name: "santos", email: "adriano@
 puts "Seeding products..."
 
 10.times do
+<<<<<<< Updated upstream
   product = Product.create!(user_id: user.id, name: sports_item.sample, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, Faker::Lorem.sentence(word_count: 20), sport: Product::SPORTS.sample)
+=======
+  product = Product.create!(user_id: user.id, name: Faker::Sport.unique..sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
+>>>>>>> Stashed changes
 
   image_url = "https://source.unsplash.com/400x400/?sport-items"
   product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 end
 
+<<<<<<< Updated upstream
 # 10.times do
 #   product = Product.create!(user_id: user2.id, name: sports_item.sample, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, Faker::Lorem.sentence(word_count: 20), sport: Product::SPORTS.sample)
+=======
+10.times do
+  product = Product.create!(user_id: user2.id, name: Faker::Sport.unique.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
+>>>>>>> Stashed changes
 
 #   image_url = "https://source.unsplash.com/400x400/?sports"
 #   product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 # end
 
+<<<<<<< Updated upstream
 # 10.times do
 #   product = Product.create!(user_id: user3.id, name: sports_item.sample, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, Faker::Lorem.sentence(word_count: 20), sport: Product::SPORTS.sample)
+=======
+10.times do
+  product = Product.create!(user_id: user3.id, name: Faker::Sport.unique.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
+>>>>>>> Stashed changes
 
 #   image_url = "https://source.unsplash.com/400x400/?sports"
 #   product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 # end
 
+<<<<<<< Updated upstream
 # 10.times do
 #   product = Product.create!(user_id: user4.id, name: sports_item.sample, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, Faker::Lorem.sentence(word_count: 20), sport: Product::SPORTS.sample)
+=======
+10.times do
+  product = Product.create!(user_id: user4.id, name: Faker::Sport.unique.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
+>>>>>>> Stashed changes
 
 #   image_url = "https://source.unsplash.com/400x400/?sports"
 #   product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
 # end
 
+<<<<<<< Updated upstream
 # 10.times do
 #   product = Product.create!(user_id: user5.id, name: sports_item.sample, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, Faker::Lorem.sentence(word_count: 20), sport: Product::SPORTS.sample)
 
 #   image_url = "https://source.unsplash.com/400x400/?sports"
 #   product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
+=======
+10.times do
+  product = Product.create!(user_id: user5.id, name: Faker::Sport.unique.sport, category: Product::CATEGORIES.sample, brand: Product::BRANDS.sample, condition: Product::CONDITION.sample, price: Faker::Commerce.price, description: Faker::Lorem.sentence, sport: Product::SPORTS.sample)
+
+#   image_url = "https://source.unsplash.com/400x400/?sport-items"
+=#   product.photo.attach(io: URI.open(image_url), filename: 'product_image.jpg')
+>>>>>>> Stashed changes
 # end
 
 puts 'Seeding completed!'
